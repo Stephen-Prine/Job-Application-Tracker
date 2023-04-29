70cb0c93cff98ca5660d1de4d0440753bf1216ff
Stephen Prine <Starvingandrew@gmail.com>
2023-04-29 16:58:57 -0400

    Initial Project Creation

---
+++ job-application-backend/src/main/java/com/stephenandrewprine/jobapplicationbackend/JobApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stephenandrewprine.jobapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ job-application-backend/src/test/java/com/stephenandrewprine/jobapplicationbackend/JobApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephenandrewprine.jobapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
